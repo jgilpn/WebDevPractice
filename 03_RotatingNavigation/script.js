59b6ba9b5abe79f3051c8baea51712c207aafaa4
John <jgilpn@gmail.com>
2021-02-21 21:16:45 -0800

    touch up

--- 03_RotatingNavigation/script.js
+++ 03_RotatingNavigation/script.js
@@ -5,12 +5,10 @@ const container = document.querySelector('.container')
 
 // Click Hamburger to Show Nav Buttons
 open.addEventListener('click', () => {
-  body.style.overflowY = 'hidden'
   container.classList.add('show-nav')
 })
 
 // Close Menu
 close.addEventListener('click', () => {
-  body.style.overflowY = 'visible'
   container.classList.remove('show-nav')
 })
