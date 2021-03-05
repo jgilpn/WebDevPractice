a14684e728e793771a4518cf01b090bec7cb73bf
John <jgilpn@gmail.com>
2021-03-04 17:11:17 -0800

    09 Sound Board - added onclick event to play/pause audio

--- 09_SoundBoard/script.js
+++ 09_SoundBoard/script.js
@@ -3,8 +3,20 @@ const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
 for (let sound of sounds) {
   const btn = document.createElement('button')
   btn.classList.add('btn')
-
   btn.innerText = sound
 
+  btn.addEventListener('click', () => {
+    stopSongs()
+    document.getElementById(sound).play()
+  })
+
   document.getElementById('buttons').appendChild(btn)
+}
+
+function stopSongs() {
+  for (let sound of sounds) {
+    const song = document.getElementById(sound)
+    song.pause()
+    song.currentTime = 0
+  }
 }
